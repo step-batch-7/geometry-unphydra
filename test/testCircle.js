402943ee272414dc3fa45b8cbd4c4891d1795597
rivu <58026024+unphydra@users.noreply.github.com>
2019-12-16 14:13:45 +0530

    more test to check toString is added

--- test/testCircle.js
+++ test/testCircle.js
@@ -3,10 +3,17 @@ const Circle = require("../src/circle");
 
 describe("circle", () => {
   describe("toString", () => {
-    it("should give to string representation of circle with valid points and radius", () => {
+    it("should give string representation of circle with valid points and radius", () => {
       const circle = new Circle({ x: 1, y: 2 }, 5);
       const actual = circle.toString();
       const expected = "[Circle @(1,2) radius 5]";
+      assert.deepStrictEqual(actual, expected);
+    });
+
+    it("should give undefined in string representation if no value is given ", () => {
+      const circle = new Circle({});
+      const actual = circle.toString();
+      const expected = "[Circle @(undefined,undefined) radius undefined]";
       assert.deepStrictEqual(actual, expected);
     });
   });
